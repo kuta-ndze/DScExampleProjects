7b5999ca51cf2c7b2c9dccfce8e8403770c1e152
Celdrick N Kuta <65413406+kuta-ndze@users.noreply.github.com>
2022-01-27 08:11:52 -0500

    rename app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-# Import required libraries
 import pandas as pd
 import dash
 import dash_html_components as html
-#from dash import html 
 import dash_core_components as dcc
-#from dash import dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
 
